7d48098b55f39be4a8c9fa5e4d3e6bedb9f351c6
Nightchild99 <dmnks.peter@gmail.com>
2023-12-22 14:35:52 +0100

    Add project files.

---
+++ dnd_manager_webapplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dnd_manager_webapplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
