7ecf52d7cbc15fbc85ce9da76a96f22e5b27ef71
Nightchild99 <dmnks.peter@gmail.com>
2023-12-22 14:39:44 +0100

    repository added

--- dnd_manager_webapplication/Data/ApplicationDbContext.cs
+++ dnd_manager_webapplication/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using dnd_manager_webapplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace dnd_manager_webapplication.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Character> Characters { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ dnd_manager_webapplication/Data/PartyRepository.cs
@@ -0,0 +1,59 @@
+﻿using dnd_manager_webapplication.Models;
+
+namespace dnd_manager_webapplication.Data
+{
+    public class PartyRepository
+    {
+        ApplicationDbContext context;
+
+        public PartyRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Character character)
+        {
+            context.Characters.Add(character);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Character> Read()
+        {
+            return context.Characters;
+        }
+
+        public Character? Read(string name)
+        {
+            return context.Characters.FirstOrDefault(t => t.Name == name);
+        }
+
+        public Character? ReadFromId(string id)
+        {
+            return context.Characters.FirstOrDefault(t => t.Id == id);
+        }
+
+        public void LevelUp(string name)
+        {
+            var character = Read(name);
+            character.Level++;
+            character.Description = $"{character.Name} is a level {character.Level} {character.Race} {character.Class}.";
+            context.SaveChanges();
+        }
+
+        public void Update(Character character)
+        {
+            var old = Read(character.Name);
+            old.Race = character.Race;
+            old.Class = character.Class;
+            old.Description = $"{character.Name} is a level {old.Level} {character.Race} {character.Class}.";
+            context.SaveChanges();
+        }
+
+        public void Delete(string name)
+        {
+            var character = Read(name);
+            context.Characters.Remove(character);
+            context.SaveChanges();
+        }
+    }
+}
