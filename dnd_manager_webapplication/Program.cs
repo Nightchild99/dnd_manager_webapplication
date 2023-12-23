0dbe58fe114bc19feca35898c7438fc862712baf
Nightchild99 <dmnks.peter@gmail.com>
2023-12-23 18:36:12 +0100

    siteuser register

--- dnd_manager_webapplication/Program.cs
+++ dnd_manager_webapplication/Program.cs
@@ -5,61 +5,6 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
-//var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddControllersWithViews();
-
-//builder.Services.AddTransient<IPartyRepository, PartyRepository>();
-
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options
-//        .UseSqlServer(connectionString)
-//        .UseLazyLoadingProxies());
-
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-//builder.Services.AddDefaultIdentity<SiteUser>(options =>
-//{
-//    options.SignIn.RequireConfirmedAccount = true;
-//    options.Password.RequireDigit = false;
-//    options.Password.RequiredLength = 8;
-//    options.Password.RequireNonAlphanumeric = false;
-//    options.Password.RequireUppercase = false;
-
-//})
-//    .AddRoles<IdentityRole>()
-//    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-
-////builder.Services.AddTransient<IEmailSender, EmailSender>();
-
-//var app = builder.Build();
-
-
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Party/Error");
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-//app.UseRouting();
-//app.UseAuthentication();
-//app.UseAuthorization();
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Party}/{action=Index}/{id?}");
-//app.MapRazorPages();
-
-//app.Run();
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -69,11 +14,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddScoped<IPartyRepository, PartyRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(opt =>
-{
     opt
        .UseSqlServer(connectionString)
-       .UseLazyLoadingProxies();
-});
+       .UseLazyLoadingProxies()
+);
 
 builder.Services.AddDefaultIdentity<SiteUser>(options =>
 {
