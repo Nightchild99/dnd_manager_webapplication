daf478e4bd7272562de803881004c3dd91eae9a5
Nightchild99 <dmnks.peter@gmail.com>
2024-01-20 17:37:16 +0100

    admin functions to different controller

---
+++ dnd_manager_webapplication/Controllers/AdminController.cs
@@ -0,0 +1,75 @@
+﻿using dnd_manager_webapplication.Data;
+using dnd_manager_webapplication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dnd_manager_webapplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IPartyRepository _repo;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<SiteUser> _userManager;
+        private readonly ILogger<PartyController> _logger;
+
+        public AdminController(IPartyRepository repo, RoleManager<IdentityRole> roleManager, UserManager<SiteUser> userManager, ILogger<PartyController> logger)
+        {
+            _repo = repo;
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> DelegateAdmin()
+        {
+            var principal = this.User;
+            var user = await _userManager.GetUserAsync(principal);
+            var role = new IdentityRole()
+            {
+                Name = "Admin"
+            };
+            if (!await _roleManager.RoleExistsAsync("Admin"))
+            {
+                await _roleManager.CreateAsync(role);
+            }
+            await _userManager.AddToRoleAsync(user, "Admin");
+            return RedirectToAction("Index", "Party");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Admin()
+        {
+            return View(this._repo.Read());
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminDelete(string name)
+        {
+            _repo.Delete(name);
+            return RedirectToAction("Index", "Party");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Users()
+        {
+            return View(_userManager.Users);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GrantAdmin(string uid)
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Id == uid);
+            await _userManager.AddToRoleAsync(user, "Admin");
+            return RedirectToAction(nameof(Users));
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RemoveAdmin(string uid)
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Id == uid);
+            await _userManager.RemoveFromRoleAsync(user, "Admin");
+            return RedirectToAction(nameof(Users));
+        }
+    }
+}
--- dnd_manager_webapplication/Controllers/PartyController.cs
+++ dnd_manager_webapplication/Controllers/PartyController.cs
@@ -23,61 +23,6 @@ namespace dnd_manager_webapplication.Controllers
             _logger = logger;
         }
 
-        #region Admin Functions
-
-        public async Task<IActionResult> DelegateAdmin()
-        {
-            var principal = this.User;
-            var user = await _userManager.GetUserAsync(principal);
-            var role = new IdentityRole()
-            {
-                Name = "Admin"
-            };
-            if (!await _roleManager.RoleExistsAsync("Admin"))
-            {
-                await _roleManager.CreateAsync(role);
-            }
-            await _userManager.AddToRoleAsync(user, "Admin");
-            return RedirectToAction(nameof(Index));
-        }
-
-        [Authorize(Roles = "Admin")]
-        public IActionResult Admin()
-        {
-            return View(this._repo.Read());
-        }
-
-        [Authorize(Roles = "Admin")]
-        public IActionResult AdminDelete(string name)
-        {
-            _repo.Delete(name);
-            return RedirectToAction(nameof(Index));
-        }
-
-        [Authorize(Roles = "Admin")]
-        public IActionResult Users()
-        {
-            return View(_userManager.Users);
-        }
-
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> GrantAdmin(string uid)
-        {
-            var user = _userManager.Users.FirstOrDefault(t => t.Id == uid);
-            await _userManager.AddToRoleAsync(user, "Admin");
-            return RedirectToAction(nameof(Users));
-        }
-
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> RemoveAdmin(string uid)
-        {
-            var user = _userManager.Users.FirstOrDefault(t => t.Id == uid);
-            await _userManager.RemoveFromRoleAsync(user, "Admin");
-            return RedirectToAction(nameof(Users));
-        }
-
-        #endregion
-
         public IActionResult Index()
         {
             return View(this._repo.Read());
